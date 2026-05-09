b1d56222fbd47a58a5489c5562d62833fd0bcf24
Divin Joseph <divinjoseph.906@gmail.com>
2026-05-09 17:20:30 +0100

    Initial Commit

---
+++ src/test/java/com/divin/devtrack/DevtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divin.devtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
